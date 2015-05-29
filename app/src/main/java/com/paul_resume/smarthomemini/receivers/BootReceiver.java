ef9ea2814c1489ba5c51773f22bba7346ffea5eb
Paul P <bigpaulie25ro@yahoo.com>
2015-05-29 18:28:27 +0300

    Adding broadcast

--- app/src/main/java/com/paul_resume/smarthomemini/receivers/BootReceiver.java
+++ app/src/main/java/com/paul_resume/smarthomemini/receivers/BootReceiver.java
@@ -3,6 +3,9 @@ package com.paul_resume.smarthomemini.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.paul_resume.smarthomemini.services.MqttService;
 
 /**
  * Created by paul on 29.05.2015.
@@ -10,6 +13,7 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Intent intent1 = new Intent(MqttService.ACTIION_NETWORK_CHANGE);
+        LocalBroadcastManager.getInstance(context).sendBroadcast(intent1);
     }
 }
